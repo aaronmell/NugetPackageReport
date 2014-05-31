20e709bdf8e5a79427222023c33bc1c148b646ae
amell <amell@alkamitech.com>
2014-05-30 20:25:39 -0500

    Slight Refactor of Report Writer to clean up some duplicated code and to prepare for additional pages

--- NugetPackageReport/ReportWriter.cs
+++ NugetPackageReport/ReportWriter.cs
@@ -12,36 +12,41 @@ namespace NugetPackageReport
     {
         public static void GenerateReport(string inputFilePath, string outputFilePath, Dictionary<PackageConfig, FeedPackage> feedPackages)
         {
-            using (var stringWriter = new StringWriter())
-            {
-                using (var writer = new HtmlTextWriter(stringWriter))
-                {
-                    writer.Write("Nuget Package Report for directory {0}", inputFilePath);
-                    writer.WriteBreak();
-                    writer.Write("Found {0} Nuget Packages", feedPackages.Count);
-                    writer.WriteBreak();
-                    writer.WriteBreak();
-
-                    writer.RenderBeginTag("table");
-
-                    WriteTableHeaders(writer, "Package Name", "Current Version", "Latest Version", "License", "Instances");
-
-                    foreach (var package in feedPackages)
-                    {
-                        WritePackageRow(writer, package);
-                    }
-
-                    writer.RenderEndTag();
-
-                    var content = writer.InnerWriter.ToString();
-                    var path = Path.Combine(outputFilePath, "Overview.html");
-                    File.WriteAllText(path, content);
-                    System.Diagnostics.Process.Start(path);
-                }
-            }
+	        GenerateGeneralPage(inputFilePath, outputFilePath, feedPackages);
         }
 
-        private static void WritePackageRow(HtmlTextWriter writer, KeyValuePair<PackageConfig, FeedPackage> package)
+		private static void GenerateGeneralPage(string inputFilePath, string outputFilePath, Dictionary<PackageConfig, FeedPackage> feedPackages)
+		{
+			using (var stringWriter = new StringWriter())
+			{
+				using (var writer = new HtmlTextWriter(stringWriter))
+				{
+					writer.Write("Nuget Package Report for directory {0}", inputFilePath);
+					writer.WriteBreak();
+					writer.Write("Found {0} Nuget Packages", feedPackages.Count);
+					writer.WriteBreak();
+					writer.WriteBreak();
+
+					writer.RenderBeginTag("table");
+
+					WriteTableHeaders(writer, "Package Name", "Current Version", "Latest Version", "License", "Instances");
+
+					foreach (var package in feedPackages)
+					{
+						WritePackageRows(writer, package);
+					}
+
+					writer.RenderEndTag();
+
+					var content = writer.InnerWriter.ToString();
+					var path = Path.Combine(outputFilePath, "Overview.html");
+					File.WriteAllText(path, content);
+					System.Diagnostics.Process.Start(path);
+				}
+			}
+		}
+
+        private static void WritePackageRows(HtmlTextWriter writer, KeyValuePair<PackageConfig, FeedPackage> package)
         {
             if (package.Value.CurrentVersion.Version != package.Value.LatestVersion.Version)
             {
@@ -49,54 +54,59 @@ namespace NugetPackageReport
             }
           
             writer.RenderBeginTag(HtmlTextWriterTag.Tr);
+	        {
+		        writer.WriteTableColumn(package.Value.CurrentVersion.Id);
 
-            writer.RenderBeginTag(HtmlTextWriterTag.Td);
-            writer.Write(package.Value.CurrentVersion.Id);
-            writer.RenderEndTag();
+		        writer.WriteTableColumn(package.Value.CurrentVersion.Version);
 
-            writer.RenderBeginTag(HtmlTextWriterTag.Td);
-            writer.Write(package.Value.CurrentVersion.Version);
-            writer.RenderEndTag();
+		        writer.WriteTableColumn(package.Value.LatestVersion.Version);
 
-            writer.RenderBeginTag(HtmlTextWriterTag.Td);
-            writer.Write(package.Value.LatestVersion.Version);
-            writer.RenderEndTag();
+		        writer.RenderBeginTag(HtmlTextWriterTag.Td);
+		        {
+			        if (!string.IsNullOrEmpty(package.Value.LatestVersion.LicenseUrl))
+			        {
+				        writer.WriteUrlLink(package.Value.CurrentVersion.LicenseUrl, "License");
+			        }
+			        else
+			        {
+				        writer.Write("No License Url");
+			        }
+		        }
+		        writer.RenderEndTag();
 
-            writer.RenderBeginTag(HtmlTextWriterTag.Td);
-            if (!string.IsNullOrEmpty(package.Value.LatestVersion.LicenseUrl))
-            {
-                writer.AddAttribute(HtmlTextWriterAttribute.Href, package.Value.CurrentVersion.LicenseUrl);
-                writer.AddAttribute(HtmlTextWriterAttribute.Target, "_blank");
-                writer.RenderBeginTag(HtmlTextWriterTag.A);
-                writer.Write("License");
-                writer.RenderEndTag();
-            }
-            else
-            {
-                writer.Write("No License Url");
-            }
-
-            writer.RenderEndTag();
-
-            writer.RenderBeginTag(HtmlTextWriterTag.Td);
-            writer.Write(package.Value.Count);
-            writer.RenderEndTag();
-
-            writer.RenderEndTag();
+		        writer.WriteTableColumn(package.Value.Count.ToString());
+	        }
+	        writer.RenderEndTag();
         }
 
         private static void WriteTableHeaders(HtmlTextWriter writer, params string[] tableHeaderNames)
         {
             writer.RenderBeginTag(HtmlTextWriterTag.Tr);
-
-            foreach (var name in tableHeaderNames)
-            {
-                writer.RenderBeginTag(HtmlTextWriterTag.Th);
-                writer.Write(name);
-                writer.RenderEndTag();
-            }
-
-            writer.RenderEndTag();
+	        {
+		        foreach (var name in tableHeaderNames)
+		        {
+			        writer.RenderBeginTag(HtmlTextWriterTag.Th);
+			        writer.Write(name);
+			        writer.RenderEndTag();
+		        }
+	        }
+	        writer.RenderEndTag();
         }
-    }
+    
+		private static void WriteTableColumn(this HtmlTextWriter writer, string valueToWrite)
+		{
+			 writer.RenderBeginTag(HtmlTextWriterTag.Td);
+             writer.Write(valueToWrite);
+             writer.RenderEndTag();
+		}
+	
+		private static void WriteUrlLink(this HtmlTextWriter writer, string url, string displayValue)
+		{
+			writer.AddAttribute(HtmlTextWriterAttribute.Href, url);
+			writer.AddAttribute(HtmlTextWriterAttribute.Target, "_blank");
+			writer.RenderBeginTag(HtmlTextWriterTag.A);
+			writer.Write(displayValue);
+			writer.RenderEndTag();
+		}
+	}
 }
